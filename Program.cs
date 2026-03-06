e3871a516d466043d23bb6c7697120df8ccde2ed
Олег Колесников <kolesnikovopera@gmail.com>
2026-03-06 20:57:35 +0000

    Initial commit: add Program
    git reset ConsoleApplication1.cpp

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+double a, b, c;
+char s;
+
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите знак действия : ");
+s = Convert.ToChar(Console.ReadLine());
+Console.Write("Введите Второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+if (s == '+')
+{
+    c = b + a;
+    Console.WriteLine(c);
+   
+    Console.ReadKey();
+}
+else if (s == '-')
+{
+    c = b - a;
+    Console.WriteLine( c);
+  
+    Console.ReadKey();
+}
+else if (s == '*')
+{
+    c = b * a;
+    Console.WriteLine( c);
+
+    Console.ReadKey();
+}
+else if (s == '/')
+{
+    if (a == 0)
+    {
+        Console.WriteLine("Ошибка");
+      
+        Console.ReadKey();
+    }
+    else
+    {
+        c = a / b;
+        Console.WriteLine(c);
+       
+        Console.ReadKey();
+    }
+
+
+}
+else
+{
+    Console.WriteLine("Ошибка. Вы ввели неверный знак.");
+    Console.WriteLine("Для выхода нажмите любую клавишу...");
+    Console.ReadKey();
+
+
+}
